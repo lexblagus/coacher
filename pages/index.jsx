e5e368ed56ddef0cbf5705b5b1ea1ae97971ee96
Lex Blagus <lex@blag.us>
2018-09-18 10:27:10 -0300

    Next working

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+export default () => <div>
+	<style global jsx>{`
+		body {
+			color: white;
+			background: black;
+		}
+	`}</style>
+	<img src="static/logo-uol.png"></img>
+	<h1>Welcome to Moving Motivators</h1>
+</div>
